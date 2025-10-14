bf3b2e3362f7b0e99b7d27619e8f0b1e80bc6905
Alex Sanchez <alex.sanchez.7e8@itb.cat>
2025-10-14 19:14:51 +0200

    fix bug

--- AEA06/routes/games.js
+++ AEA06/routes/games.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
     const data = readData();
     res.render("games", { user, data, htmlMessage });
 });
-router.get('/editJoc/:id', (req, res) => {
+router.get('/editGame/:id', (req, res) => {
     const user = { name: "Alex" };
     const htmlMessage = `
     <p>Aquest és un text <strong>amb estil</strong> i un enllaç:</p>
