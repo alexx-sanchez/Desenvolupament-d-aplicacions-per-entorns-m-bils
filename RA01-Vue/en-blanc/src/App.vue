f763da72836c0d6aba875b3e26cee3962a439a8c
Alex Sanchez <alex.sanchez.7e8@itb.cat>
2025-10-23 16:47:51 +0200

    update 3 examples

---
+++ RA01-Vue/en-blanc/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import CanviMissatge from './CanviMissatge.vue'
+import Contador from './Contador.vue'
+import DataBinding from './DataBinding.vue';
+</script>
+
+<template>
+  <div class="app-container">
+    <CanviMissatge />
+    <Contador />
+    <DataBinding />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* 3 columnas iguales */
+  gap: 20px; /* espacio entre los componentes */
+  justify-items: center; /* centra los elementos en cada columna */
+  align-items: start; /* alinea al inicio verticalmente */
+  padding: 20px;
+}
+
+</style>
---
+++ RA01-Vue/en-blanc/src/CanviMissatge.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const missatge = ref('Hola, món!')
+const canviarMissatge = () => {
+  missatge.value = 'Hola, Vue'
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ missatge }}</h1>
+    <button @click="canviarMissatge">Canvia el missatge</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #cce7ff; /* fondo suave */
+  border-radius: 10px;
+  text-align: center;
+}
+
+h1 {
+  font-size: 1.5rem;
+  margin-bottom: 15px;
+  color: #222;
+}
+
+button {
+  padding: 8px 16px;
+  font-size: 1rem;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  background-color: #0077b6;
+  color: white;
+}
+
+button:hover {
+  background-color: #0096c7;
+}
+</style>
