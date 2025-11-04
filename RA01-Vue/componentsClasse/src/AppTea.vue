1ddc7db4467d5378b57535ca7a72d61b5152d924
Alex Sanchez <alex.sanchez.7e8@itb.cat>
2025-11-04 19:09:40 +0100

    upload AppTea

---
+++ RA01-Vue/componentsClasse/src/AppTea.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Fill from './components/Fill.vue';
+
+const colores = ['Verds', 'Vermells', 'Negre', 'Groc']
+</script>
+
+<template>
+    <div class="content">
+        <h1 class="title">Subscripcions disponibles</h1>
+        <h2 class="subtitle">
+            Disposem dels millor tes del món
+        </h2>
+
+        <div class="plans">
+            <Fill v-for="color in colores" :key="color" :nom="color" />
+        </div>
+    </div>
+</template>
---
+++ RA01-Vue/componentsClasse/src/components/Fill.vue
@@ -0,0 +1,14 @@
+<script setup>
+const prop = defineProps({
+    nom: String
+})
+</script>
+
+<template>
+    <div class="plan">
+        <div class="description">
+            <span class="title"> {{ nom }}</span>
+        </div>
+    </div>
+
+</template>
