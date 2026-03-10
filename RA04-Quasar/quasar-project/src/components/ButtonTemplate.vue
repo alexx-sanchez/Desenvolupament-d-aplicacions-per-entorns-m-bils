fa1d9671157608c7aa7c3f9ef045fbb97de1e851
Alex sanchez <alex.sanchez.7e8@itb.cat>
2026-03-10 19:10:22 +0100

    adding ra4

---
+++ RA04-Quasar/quasar-project/src/components/ButtonTemplate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <q-toggle
+      v-model="value"
+      color="primary"
+      keep-color
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      value: ref(false)
+    }
+  }
+}
+</script>
---
+++ RA04-Quasar/quasar-project/src/pages/LlistatPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    listId: {
+        required: true,
+        type: String
+    }
+})
+</script>
+
+<template>
+    listId: {{ listId }}
+    <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+    </ul>
+</template>
